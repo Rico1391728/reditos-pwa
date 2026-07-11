d0ceb9065f8fa7c24bf8ac4be8977fb01c79209e
Rico1391728 <rixocastillo@gmail.com>
2026-07-11 01:07:23 -0400

    Add sw.js - Service Worker

---
+++ sw.js
@@ -0,0 +1,42 @@
+// Mis Réditos - Service Worker v1
+var CACHE = 'reditos-v1';
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll([
+        // La app se cachea sola dinámicamente en fetch
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE; })
+          .map(function(k) { return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  // Solo interceptar requests al mismo origen (share.zapia.com)
+  if (e.request.url.startsWith(self.location.origin)) {
+    e.respondWith(
+      caches.open(CACHE).then(function(cache) {
+        return cache.match(e.request).then(function(response) {
+          var fetchPromise = fetch(e.request).then(function(networkResponse) {
+            cache.put(e.request, networkResponse.clone());
+            return networkResponse;
+          });
+          return response || fetchPromise;
+        });
+      })
+    );
+  }
+});
